67af408a3deadb7580eacbeacfcad82b2e0446ef
SebastianUD <spardot@correo.udistrital.edu.co>
2022-11-02 17:34:33 -0500

    Primer_commit

---
+++ clases_objetos.py
@@ -0,0 +1,38 @@
+class Persona:
+    def __init__(self):
+        self.nombre = ""
+        self.edad = 0
+        self.genero = ""
+        self.volumen = ""
+        self.bigote = False
+    
+    def gritar(self):
+        pass
+    
+    def bailar(self):
+        pass
+    
+    def saltar(self):
+        pass
+
+sujeto_a=Persona()
+sujeto_a.bigote=False
+sujeto_a.nombre= "Camilo"
+sujeto_a.edad=20
+sujeto_a.genero="pansexual"
+sujeto_a.volumen= "alto"
+
+sujeto_b=Persona()
+sujeto_b.bigote=False
+sujeto_b.nombre="cristian"
+sujeto_b.edad=56
+sujeto_b.genero="masculino"
+sujeto_b.volumen="medio"
+
+sujeto_c=Persona()
+sujeto_c.bigote= False
+sujeto_c.nombre="Lina"
+sujeto_c.edad=19
+sujeto_c.genero="femenino"
+sujeto_c.volumen="medio"
+
